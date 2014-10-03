bb8959c7529b7f02ace0b97a6f7c7c9b1496eea6
Kirk Wight <kwight@kwight.ca>
2014-10-02 21:04:21 -0700

    Fix up empty docblocks.

--- eventbrite-api.php
+++ eventbrite-api.php
@@ -12,10 +12,6 @@ License: GPL v2 or newer <https://www.gnu.org/licenses/gpl.txt>
 
 /**
  * Load our API on top of the Keyring Eventbrite service.
- *
- * @param
- * @uses
- * @return
  */
 function eventbrite_api_load_keyring_service() {
 	require_once( 'inc/class-eventbrite-api.php' );
@@ -25,9 +21,7 @@ add_action( 'keyring_load_services', 'eventbrite_api_load_keyring_service', 11 )
 /**
  * Load classes and helper functions.
  *
- * @param
- * @uses
- * @return
+ * @uses current_theme_supports()
  */
 function eventbrite_api_init() {
 	// Load our plugin only if the current theme supports it.
