5eb58dbd4b7e7387d4a843d96cd5641dbd092418
Kirk Wight <kwight@kwight.ca>
2014-10-30 15:04:18 -0700

    Allow themes to define an Eventbrite event template part for content, without having to add template files.

--- tmpl/eventbrite-index.php
+++ tmpl/eventbrite-index.php
@@ -31,7 +31,7 @@ get_header(); ?>
 						 * use this in a child theme, then include a file called called content-___.php
 						 * (where ___ is the post format) and that will be used instead.
 						 */
-						get_template_part( 'content' );
+						get_template_part( 'content', 'eventbrite' );
 
 					endwhile;
 
