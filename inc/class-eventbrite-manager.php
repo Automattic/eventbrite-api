Automattic/eventbrite-api

commit ec5a89a10ea2d4e65c0213854efba05abd075d47

Simplify logic for parameter validation. Props jkudish.
See: https://github.com/Automattic/eventbrite-api/commit/f440e4b4dac88ee153310f555d0b83a6af6db4a6#commitcomment-849609

inc/class-eventbrite-manager.php
@@ -111,8 +111,7 @@ class Eventbrite_Manager {
 
 		// Compare each passed parameter and value against our valid ones, and fail if a match can't be found.
 		foreach ( $params as $key => $value ) {
-			if ( ! array_key_exists( $key, $valid[$endpoint] ) ||
-				( ! empty( $valid[$endpoint][$key] ) && ! in_array( $value, $valid[$endpoint][$key] ) ) ) {
+			if ( empty( $valid[$endpoint][$key] ) || ! in_array( $value, $valid[$endpoint][$key] ) ) {
 				return false;
 			}
 		}

commit aec9073b6e847d28f4a48a1b65f59b9ae9175c59

Correct logic for parameter validation in a more readable way.
Fixes validation issues introduced by ec5a89a10ea2d4e65c0213854efba05abd075d47.

First, the parameter itself must be valid for that endpoint. Second, if that parameter has a set number of valid values, the passed-in value needs to be validated.

inc/class-eventbrite-manager.php
@@ -111,7 +111,13 @@ class Eventbrite_Manager {
 
 		// Compare each passed parameter and value against our valid ones, and fail if a match can't be found.
 		foreach ( $params as $key => $value ) {
-			if ( empty( $valid[$endpoint][$key] ) || ! in_array( $value, $valid[$endpoint][$key] ) ) {
+			// Check the parameter is valid for that endpoint.
+			if ( ! array_key_exists( $key, $valid[$endpoint] ) ) {
+				return false;
+			}
+
+			// If the parameter has a defined set of possible values, make sure the passed value is valid.
+			if ( ! empty( $valid[$endpoint][$key] ) && ! in_array( $value, $valid[$endpoint][$key] ) ) {
 				return false;
 			}
 		}