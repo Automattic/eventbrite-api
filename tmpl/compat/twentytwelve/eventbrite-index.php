4cbb246bf25b7a94b8ee9ce673eef157669b2058
Kirk Wight <kwight@kwight.ca>
2014-11-16 13:23:53 -0800

    Add compat files for the Twenty Twelve default theme.

---
+++ tmpl/compat/twentytwelve/eventbrite-index.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * Template Name: Eventbrite Events
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+			<header class="archive-header">
+				<h1 class="archive-title">
+					<?php the_title(); ?>
+				</h1>
+			</header><!-- .archive-header -->
+
+			<?php
+				// Set up and call our Eventbrite query.
+				$events = new Eventbrite_Query( apply_filters( 'eventbrite_query_args', array(
+					'display_private' => true, // boolean
+					// 'limit' => null,            // integer
+					// 'organizer_id' => null,     // integer
+					// 'p' => null,                // integer
+					// 'post__not_in' => null,     // array of integers
+					// 'venue_id' => null,         // integer
+				) ) );
+
+				if ( $events->have_posts() ) :
+					while ( $events->have_posts() ) : $events->the_post(); ?>
+
+						<article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<header class="entry-header">
+								<?php the_post_thumbnail(); ?>
+
+								<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+								<div class="entry-meta">
+									<?php eventbrite_event_meta(); ?>
+
+									<?php eventbrite_edit_post_link( __( 'Edit', 'eventbrite_api' ), '<span class="meta-separator"> &middot; </span><span class="edit-link">', '</span>' ); ?>
+								</div><!-- .entry-meta -->
+							</header><!-- .entry-header -->
+
+							<div class="entry-content">
+								<?php eventbrite_ticket_form_widget(); ?>
+							</div><!-- .entry-content -->
+						</article><!-- #post-## -->
+
+					<?php endwhile;
+
+					// Previous/next post navigation.
+					eventbrite_paging_nav( $events );
+
+				else :
+					// If no content, include the "No posts found" template.
+					get_template_part( 'content', 'none' );
+
+				endif;
+
+				// Return $post to its rightful owner.
+				wp_reset_postdata();
+			?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
