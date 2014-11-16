9e98a4997ebb4a05a7c1532a7aa1ca36ad1c14fd
Kirk Wight <kwight@kwight.ca>
2014-11-16 10:20:01 -0800

    Add compat files for the Twenty Fourteen default theme.

---
+++ tmpl/compat/twentyfourteen/eventbrite-index.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Template Name: Eventbrite Events
+ */
+
+get_header(); ?>
+
+<div id="main-content" class="main-content">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<header class="page-header">
+				<h1 class="page-title">
+					<?php the_title(); ?>
+				</h1>
+			</header><!-- .page-header -->
+
+			<?php
+				// Set up and call our Eventbrite query.
+				$events = new Eventbrite_Query( apply_filters( 'eventbrite_query_args', array() ) );
+
+				if ( $events->have_posts() ) :
+					while ( $events->have_posts() ) : $events->the_post(); ?>
+
+						<article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<?php the_post_thumbnail(); ?>
+
+							<header class="entry-header">
+
+								<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+
+								<div class="entry-meta">
+									<?php eventbrite_event_meta(); ?>
+
+									<?php eventbrite_edit_post_link( __( 'Edit', 'eventbrite_api' ), '<span class="edit-link">', '</span>' ); ?>
+								</div><!-- .entry-meta -->
+							</header><!-- .entry-header -->
+
+							<div class="entry-content">
+								<?php eventbrite_ticket_form_widget(); ?>
+							</div><!-- .entry-content -->
+						</article><!-- #event-## -->
+
+					<?php endwhile;
+
+					// Previous/next post navigation.
+					eventbrite_paging_nav( $events );
+
+				else :
+					// If no content, include the "No posts found" template.
+					get_template_part( 'content', 'none' );
+
+				endif;
+
+				// Return $post to its rightful owner.
+				wp_reset_postdata();
+			?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+	<?php get_sidebar( 'content' ); ?>
+</div><!-- #main-content -->
+
+<?php
+get_sidebar();
+get_footer();
---
+++ tmpl/compat/twentyfourteen/eventbrite-single.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * The Template for displaying all single Eventbrite events.
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				// Get our event based on the ID passed by query variable.
+				$event = new Eventbrite_Query( array( 'p' => get_query_var( 'eventbrite_id' ) ) );
+
+				if ( $event->have_posts() ) :
+					while ( $event->have_posts() ) : $event->the_post(); ?>
+
+						<article id="event-<?php the_ID(); ?>" <?php post_class(); ?>>
+							<div class="post-thumbnail">
+								<?php the_post_thumbnail(); ?>
+							</div>
+
+							<header class="entry-header">
+
+								<h1 class="entry-title"><?php the_title(); ?></h1>
+
+								<div class="entry-meta">
+									<?php eventbrite_event_meta(); ?>
+
+									<?php eventbrite_edit_post_link( __( 'Edit', 'eventbrite_api' ), '<span class="edit-link">', '</span>' ); ?>
+								</div><!-- .entry-meta -->
+							</header><!-- .entry-header -->
+
+							<div class="entry-content">
+								<?php the_content(); ?>
+
+								<?php eventbrite_ticket_form_widget(); ?>
+							</div><!-- .entry-content -->
+						</article><!-- #event-## -->
+
+					<?php endwhile;
+
+				else :
+					// If no content, include the "No posts found" template.
+					get_template_part( 'content', 'none' );
+
+				endif;
+
+				// Return $post to its rightful owner.
+				wp_reset_postdata();
+			?>
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar( 'content' );
+get_sidebar();
+get_footer();
